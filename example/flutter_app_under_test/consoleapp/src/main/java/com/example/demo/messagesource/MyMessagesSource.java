8406aadb844166c0c756962f6e7be24e1cb3f949
Dmitriy <dima.abdurazakov.92@gmail.com>
2021-02-10 01:36:45 +0300

    tmp commit

--- example/flutter_app_under_test/consoleapp/src/main/java/com/example/demo/messagesource/MyMessagesSource.java
+++ example/flutter_app_under_test/consoleapp/src/main/java/com/example/demo/messagesource/MyMessagesSource.java
@@ -1,14 +1,9 @@
 package com.example.demo.messagesource;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.filter.OrderedCharacterEncodingFilter;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-
-import java.util.Locale;
 
 @Configuration
 public class MyMessagesSource {
--- example/flutter_app_under_test/consoleapp/src/main/java/com/example/demo/repositories/UserRepository.java
+++ example/flutter_app_under_test/consoleapp/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -4,6 +4,4 @@ import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User save(User user);
-
 }
